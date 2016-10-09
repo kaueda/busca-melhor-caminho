a6fa84932e09bb33950a2bff349c7eb973e35919
kaueda <kauesilveira1609@gmail.com>
2016-10-09 01:52:16 -0300

    added bfs implementation

--- scripts/main.js
+++ scripts/main.js
@@ -8,30 +8,87 @@ var GameState = {
         this.map = this.add.tilemap('map1');
         this.map.addTilesetImage('tiles', 'tiles');
 
-        this.layer = this.map.createLayer('terrain');
-        this.layer.resizeWorld();
-        this.layer.wrap = true;
+        this.main = this.map.createLayer('main');
+        this.main.resizeWorld();
+        this.main.wrap = true;
 
-        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.over = this.map.createBlankLayer('over', 10, 10, 32, 32);
+        this.over.alpha = 0.2;
+        
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        for (i in this.map.tiles) this.map.tiles[i].visited = 0;
+        this.bfs(this.map.searchTileIndex(tiles.start))
     },
 
     update: function() {
         if (this.input.mousePointer.isDown) {
-            this.map.putTile(this.tiles.finished, 
-                             this.layer.getTileX(this.input.worldX), 
-                             this.layer.getTileY(this.input.worldY));
+            this.over.putTile(this.tiles.finished, 
+                             this.main.getTileX(this.input.worldX), 
+                             this.main.getTileY(this.input.worldY));
+        }
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            // Itera sobre o algoritmo e atualiza o mapa
         }
     },
 
+    findNeighbors: function(tile) {
+        var ans = new Array()
+
+        if(tile.x + 1 < this.map.width)
+            ans.push(new Phaser.Point(tile.x + 1, tile.y));
+
+        if(tile.x - 1 > 0)
+            ans.push(new Phaser.Point(tile.x - 1, tile.y));
+        
+        if(tile.y + 1 < this.map.height)
+            ans.push(new Phaser.Point(tile.x, tile.y + 1));
+        
+        if(tile.y - 1 > 0)
+            ans.push(new Phaser.Point(tile.x, tile.y - 1));
+        
+        return ans 
+    },
+
+    bfs: function(start) {
+        var queue = new Array();
+
+        start.visited = 2;
+        queue.push(start);
+
+        while (queue.length > 0) {
+            current = queue.shift();
+
+            neighbors = findNeighbors(current)
+            for (i in neighbors) {
+                neighbor = this.map.getTile(neighbors[i].x, neighbors[i].y)
+                if (neighbor.visited != 2) {
+                    neighbor.visited = 2;
+                    neighbor.traceback = current;
+                    // pinta de amarelo
+                    //this.over.putTile(this.tiles.visited, neighbor.x, neighbor.y);
+                    queue.push(neighbor);
+                }
+            }
+
+            // pinta de azul
+            //this.over.putTile(this.tiles.visited, current.x, current.y);
+        }
+    },
+
+    a_star: function(start, end) {
+
+    },
+
     tiles: {
+        finished: 1,
         start:    2,
-        end:      5,
-        wall:     7,
-        wall2:    8,
-        mud:      4,
         empty:    3,
+        mud:      4,
+        end:      5,
         visited:  6,
-        finished: 1
+        wall:     7,
+        wall2:    8
     }
 };
 
